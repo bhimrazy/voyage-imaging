72b6721e36f6600446e03f9843431b85c8297761
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2023-01-15 15:40:54 +0000

    Added some directories and initial setup
    Some added libraries:
    - fastapi
    - uvicorn
    - gunicorn
    - alembic
    - flake, black, isort, etc.
    
    Folder Structure:
        .
        ├── alembic/            alembic database migrations
        ├── src/                main app logic with MVC, etc
        │   └── main.py
        ├── tests/              tests
        ├── manage.py           manager script
        ├── requirements.txt
        ├── alembic.ini
        └── README.md

---
+++ backend/README.md
@@ -0,0 +1,43 @@
+# Voyage Imaging
+
+## Installation and Setup
+
+```
+    #create a python environment
+    $ python -m venv venv
+    #activate environment
+    $ source venv/bin/activate # use venv/Scripts/activate for windows
+    #install packages from requirements.txt file
+    $ pip install -r requirements.txt
+
+    # Run app
+    $ uvicorn src.main:app --reload # dev
+    $ uvicorn src.main:app # prod
+
+    # Run test and coverage
+    $ pytest
+    $ coverage run -m pytest
+    $ coverage report -i
+
+    # Run flake8
+    $ flake8
+
+```
+
+## Project Structure
+```
+    .
+    ├── alembic/            alembic database migrations
+    ├── src/                main app logic with MVC, etc   
+    │   └── main.py
+    ├── tests/              tests 
+    ├── manage.py           manager script
+    ├── requirements.txt    
+    ├── alembic.ini
+    └── README.md
+
+```
+
+## Authors
+
+## Resources
---
+++ backend/manage.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from typing import List
+
+COMMANDS = {"serve": "Start the server.", "test": "Run the tests"}
+
+
+def generate_description_of_commands() -> None:
+    """Generates the description of all the availabe commands."""
+    print("python manage.py\t[COMMAND]")
+    print(f'COMMANDS\t\tDESCRIPTION\n{"-"*8}\t\t{"-"*11}')
+    for cmd, des in COMMANDS.items():
+        print(f"{cmd}\t\t\t{des}")
+
+
+def handle_commands(args: List[str]) -> None:
+    """This function handles all the commands.
+    Args:
+        args (List[str]): List of commands
+    """
+    cmd = args[1]
+    if cmd not in COMMANDS:
+        raise Exception("Not a valid command")
+
+    if cmd == "serve":
+        os.system("uvicorn src.main:app --host 0.0.0.0 --reload")
+    elif cmd == "test":
+        os.system("pytest")
+    else:
+        print("Nothing")
+
+
+if __name__ == "__main__":
+
+    # parse command line args
+    args = sys.argv
+
+    if len(args) < 2:
+        generate_description_of_commands()
+    else:
+        handle_commands(args)
